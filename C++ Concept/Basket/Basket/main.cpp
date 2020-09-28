1ac717fb75958d5323eb1c9996508e9cb04ffc2f
ShinkuKira21 <crazygaming055@gmail.com>
2020-09-28 23:19:16 +0100

    Created Plan to Basket Remove
    Implementing shortly.

--- C++ Concept/Basket/Basket/main.cpp
+++ C++ Concept/Basket/Basket/main.cpp
@@ -1,3 +1,7 @@
+/* Author: Edward Patch */
+
+//MY MESSY MAIN NOTEPAD.
+
 #include "BasketInformation.h"
 
 std::string** CBInformation::products;
@@ -5,44 +9,73 @@ int CBInformation::productCount;
 
 int main()
 {
-	std::string** hello;
+	std::string** input;
 	int size[2] = { 1, 3 };
 
 
-	hello = new std::string*[size[1]];
+	input = new std::string*[size[1]];
 
 	for (int i = 0; i < size[1]; i++)
 	{
-		hello[i] = new std::string[size[0]];
+		input[i] = new std::string[size[0]];
 
 		for (int j = 0; j < size[0]; j++)
-			getline(std::cin, hello[i][j]);
+			getline(std::cin, input[i][j]);
 	}
 
-	CBInformation* cb = new CBInformation(hello, size, "Add");
+	CBInformation* cb = new CBInformation(input, size, "Add");
 
-	delete[] hello;
+	delete[] input;
 
-	hello = new std::string * [size[1]];
+	input = new std::string * [size[1]];
 
 	for (int i = 0; i < size[1]; i++)
 	{
-		hello[i] = new std::string[size[0]];
+		input[i] = new std::string[size[0]];
 
 		for (int j = 0; j < size[0]; j++)
-			getline(std::cin, hello[i][j]);
+			getline(std::cin, input[i][j]);
 	}
 
-	cb = new CBInformation(hello, size, "Add");
+	cb = new CBInformation(input, size, "Add");
 
-	std::string** test = CBInformation::GetCBInformation();
+	delete[] input;
+
+	input = new std::string * [size[1]];
+
+	for (int i = 0; i < size[1]; i++)
+	{
+		input[i] = new std::string[size[0]];
+
+		for (int j = 0; j < size[0]; j++)
+			getline(std::cin, input[i][j]);
+	}
+
+	cb = new CBInformation(input, size, "Remove");
+
+	delete[] input;
+
+
+	input = new std::string * [size[1]];
+
+	for (int i = 0; i < size[1]; i++)
+	{
+		input[i] = new std::string[size[0]];
+
+		for (int j = 0; j < size[0]; j++)
+			getline(std::cin, input[i][j]);
+	}
+
+	cb = new CBInformation(input, size, "Add");
+
+	delete[] input;
+
+	std::string** output = CBInformation::GetCBInformation();
 	int newSize[2] = { CBInformation::GetProductRows(), 3 };
 
 	for (int i = 0; i < newSize[0]; i++)
 		for (int j = 0; j < newSize[1]; j++)
-			std::cout << test[j][i];
-
-	delete[] hello;
+			std::cout << output[j][i];
 
 	return 0;
 }
